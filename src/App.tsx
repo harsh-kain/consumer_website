7f1eac1f50acdb6839bd7c534677b20653309ff8
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-04-21 09:02:30 +0530

    Setting up Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <div>Amrutam Pharmaceuticals</div>;
+  return (
+    <h1 className="text-3xl font-bold underline">Amrutam Pharmaceuticals</h1>
+  );
 }
 
 export default App;
